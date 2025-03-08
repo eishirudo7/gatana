cecc74f7d1a5fa9ebd7f824f676aad78d6f42c23
eishirudo7 <eishirudo@gmail.com>
2025-03-09 00:31:18 +0700

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  output: 'standalone',
+  reactStrictMode: true,
+  swcMinify: true,
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
